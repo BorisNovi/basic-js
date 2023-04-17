9ec46c315c92079f8e611c5ed0048a003840ff1a
BorisNovi <112634761+BorisNovi@users.noreply.github.com>
2023-04-17 19:18:09 +0400

    feat: make m-el

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,10 +16,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+
+  for (let j = 0; j < matrix[0].length; j++) {
+    for (let i = 0; i < matrix.length; i++) {
+      if (i === 0 || matrix[i - 1][j] !== 0) {
+        sum += matrix[i][j];
+      }
+    }
+  }
+  console.log(matrix, 'after 0 = ', sum);
+  return sum;
 }
+
+
+getMatrixElementsSum([
+  [0, 1, 1, 2],
+  [0, 5, 0, 0],
+  [2, 0, 3, 3],
+]); // return 9
+
+getMatrixElementsSum([
+    [1, 2, 3, 4],
+    [0, 5, 0, 0],
+    [2, 0, 3, 3],
+  ]); // return 15
+
+  getMatrixElementsSum([
+    [1, 1, 1],
+    [2, 2, 2],
+    [3, 3, 3],
+  ]); // return 18
+
+  getMatrixElementsSum([
+    [0],
+  ]); // return 0
+
+  getMatrixElementsSum([
+    [1],
+    [5],
+    [0],
+    [2],
+  ]); // return 6
 
 module.exports = {
   getMatrixElementsSum
