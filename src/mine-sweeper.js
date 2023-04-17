4128fe683f48334706838aab82c09e45a281d385
BorisNovi <112634761+BorisNovi@users.noreply.github.com>
2023-04-17 22:39:20 +0400

    feat: make m-sweep and chain

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,10 +23,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  const result = [];
+
+  for (let i = 0; i < rows; i++) {
+    result.push([]);
+    for (let j = 0; j < cols; j++) {
+      let count = 0;
+
+      for (let x = i - 1; x <= i + 1; x++) {
+        for (let y = j - 1; y <= j + 1; y++) {
+          if (x >= 0 && x < rows && y >= 0 && y < cols && !(x === i && y === j)) {
+            count += matrix[x][y] ? 1 : 0;
+          }
+        }
+      }
+      result[i][j] = count;
+    }
+  }
+
+  return result;
 }
+
+const matrix = [
+  [true, false, false],
+  [false, true, false],
+  [false, false, false]
+];
+
+console.log(minesweeper(matrix));
 
 module.exports = {
   minesweeper
